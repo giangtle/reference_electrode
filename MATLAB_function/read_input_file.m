a2c8d7faba8d580c9b009af2193ed241a6b0fe12
giangtle <giangtle@uw.edu>
2023-10-04 21:48:30 -0700

    fix matlab function

--- MATLAB_function/read_input_file.m
+++ MATLAB_function/read_input_file.m
@@ -1,12 +1,12 @@
 function [param_names, unit_names, params_matrix] = read_input_file(file_name)
-arguments
-    file_name char = './model_input/inputs.txt'
-end
+    arguments
+        file_name char = './model_input/inputs.txt'
+    end
 % Pass the unchanged input:
 fid=fopen(file_name,'r');
 param_names = strsplit(fgetl(fid),"\t");
 unit_names = strsplit(fgetl(fid),"\t");
 fclose(fid);
-params_matrix = readmatrix('./model_input/inputs.txt','NumHeaderLines',2);
+params_matrix = readmatrix(file_name,'NumHeaderLines',2);
 end
 
