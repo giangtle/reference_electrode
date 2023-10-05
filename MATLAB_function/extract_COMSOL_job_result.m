2e9b71b9079eb1993455d88513fbb73b95649862
giangtle <giangtle@uw.edu>
2023-10-04 19:21:56 -0700

    add i_Sigma to EIS simulation exporting, set up second round of EIS simulation

--- MATLAB_function/extract_COMSOL_job_result.m
+++ MATLAB_function/extract_COMSOL_job_result.m
@@ -3,16 +3,17 @@ if strcmp(model_name, 'EIS_model.mph')
     % Get psi value
     table2 = mphtable(temp_model, 'tbl2');
     table2 = table2.data;
-    fprintf(fid,'%.4g\t', [table2(1,2); table2(1,3)]);
-    % Get psi11 value
+    % Get steady state value in order: i_Sigma, psi_ref_o1, psi_ref_o2
+    fprintf(fid,'%.4g\t', [table2(1,4); table2(1,2); table2(1,3)]);
+    % Get harmonic value
     table1 = mphtable(temp_model, 'tbl1');
     table1 = table1.data;
-    psi_ref_o1 = table1(:,2).';
-    psi_ref_o2 = table1(:,3).';
-    i_Sigma = table1(:,4).';
-    fprintf(fid,'%.4g+%.4gj\t',[real(i_Sigma);imag(i_Sigma)]);
-    fprintf(fid,'%.4g+%.4gj\t',[real(psi_ref_o1);imag(psi_ref_o1)]);
-    fprintf(fid,'%.4g+%.4gj\t',[real(psi_ref_o2);imag(psi_ref_o2)]);
+    psi11_ref_o1 = table1(:,2).';
+    psi11_ref_o2 = table1(:,3).';
+    i_Sigma11 = table1(:,4).';
+    fprintf(fid,'%.4g+%.4gj\t',[real(i_Sigma11);imag(i_Sigma11)]);
+    fprintf(fid,'%.4g+%.4gj\t',[real(psi11_ref_o1);imag(psi11_ref_o1)]);
+    fprintf(fid,'%.4g+%.4gj\t',[real(psi11_ref_o2);imag(psi11_ref_o2)]);
 elseif strcmp(model_name,'iV_model.mph')
     % Get gamma & psi value
     table1 = mphtable(temp_model, 'tbl1');
